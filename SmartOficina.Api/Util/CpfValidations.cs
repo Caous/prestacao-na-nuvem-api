ff898ac2686c42caa6686a7384c5e99fcf12629e
caous <caous.g@gmail.com>
2023-12-14 13:26:48 -0300

    feat: Alterações na controller de cliente para cadastro

---
+++ SmartOficina.Api/Util/CpfValidations.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SmartOficina.Api.Util;
+
+public abstract class CpfValidations
+{
+    Cpf _cpf;
+
+    public static string NumeroCpf { get; private set; }
+
+    public static string CpfSemPontuacao(string numeroCpf) {
+
+        if (numeroCpf.IsMissing())
+        {
+            return string.Empty;
+        }
+        NumeroCpf = Regex.Replace(numeroCpf, @"[^\d]", "");
+
+        return NumeroCpf;
+    }
+}
---
+++ SmartOficina.Api/Util/RgValidations.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SmartOficina.Api.Util;
+
+public abstract class RgValidations
+{
+    public static string RemoverPontuacaoRg(string rg)
+    {
+        if (rg.IsMissing())
+        {
+            return string.Empty;
+        }
+        return Regex.Replace(rg, @"[^\d]", ""); ;
+    }
+}
